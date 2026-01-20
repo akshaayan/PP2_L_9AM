746482f416bc07a9e07654f83d5d8f9937a6402f
akshaayan <akshaayan@gmail.com>
2026-01-20 10:53:05 +0500

    first commit

---
+++ test.py
@@ -0,0 +1,20 @@
+x = 1 + 2 * 3 - 4 / 5 ** 6
+z = 5**6 
+# s = 4/15625
+# v = 1+6-s
+# print(v)
+d = 23.9
+print(type(d))
+d = str(d)
+print(type(d))
+
+# nam = input('Who are you? ')
+# print('Welcome', nam)
+
+wh = float(input('enter Working hours '))
+rt = float(input('enter Rate')) 
+pay = wh*rt
+pay_str= str(pay)
+print("Pays: ", pay_str)
+
+print("test msg")
