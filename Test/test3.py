1099ac8fd2cb6e3dd4a9917e9c63287f0517b199
akshaayan <akshaayan@gmail.com>
2026-01-27 11:18:41 +0500

    lecture file upload

---
+++ Test/test3.py
@@ -0,0 +1,24 @@
+# test = [3, [3, 6, 7], ['string'], 6]
+# a = test[2][0]
+# print(type(a))
+# test[3]=10
+# test.append([2, 4])
+# test.pop(0)
+# print(test) 
+# s = 'Hello'
+# for i in range(len(s)): 
+#     print(s[i])
+    
+students = ['Alex', 'Chris', 'Monika']
+grades = [16, 67, 90]
+grades.insert(1, 100)
+# print(grades)
+# z = sum(students)
+# print(z)
+
+f = 'test test2, te,st3'
+a = f.split(',')
+print(a)
+
+# for i in students: 
+#     print(i, 'got: ', grades[i]) 
